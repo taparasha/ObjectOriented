e07aac2f3a0122b98fd8a258753fc1cea64e513d
yoavhenig <33389702+yoavhenig@users.noreply.github.com>
2017-11-23 14:11:29 +0200

    Update WifiNetworkExportTest.java
    added fields to test

--- ObjectOriented/src/matala0/WifiNetworkExportTest.java
+++ ObjectOriented/src/matala0/WifiNetworkExportTest.java
@@ -30,15 +30,23 @@ public class WifiNetworkExportTest {
 	public void testBuildWifinetworkToExport() {
 		
 		WifiNetworkImport act = new WifiNetworkImport();
-        act.setMAC("test");
+       		act.setMAC("test");
+		act.setSSID("test");
+		act.setAccuracyMeters(0);
+		act.setRSSI(-3)
+	
 		
-        WifiNetworkExport ex = new WifiNetworkExport();
-        ex.setMAC("test");
+		
+        	WifiNetworkExport ex = new WifiNetworkExport();
+        	ex.setMAC("test");
+		ex.setSSID("test");
+		ex.setFreuncy(0);
+		ex.setSignal(-3);
         
         WifiNetworkExport actual = WifiNetworkExport.buildWifinetworkToExport(act);
         
-        String s = ""+actual.getMAC();
-        String t = ""+ex.getMAC();
+        String s = ""+actual.getMAC()+actual.getSSID()+actual.getFreuncy()+actual.getSignal();
+        String t = ""+ex.getMAC()+ex.getSSID()+ex.getFreuncy()+ex.getSignal();;
         
 		assertEquals(t, s);
 	}
