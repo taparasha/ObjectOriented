bcb0ba433aeceef91bd398d539d15c6732f811a9
yoavhenig <yoavhenig@DESKTOP-5UB08FL>
2017-11-23 20:38:25 +0200

    Added new test for the DataToExport function

--- ObjectOriented/src/matala0/DataToExportTest.java
+++ ObjectOriented/src/matala0/DataToExportTest.java
@@ -3,6 +3,8 @@ package matala0;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.After;
@@ -73,13 +75,35 @@ public class DataToExportTest {
 
 	@Test
 	public void testBuildCSVData() {
+		
+		Date time = new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.set(2017, 9, 28, 20, 10, 00);
+		time=cal.getTime();
+		
 		DataToExport ac = new DataToExport();
+		ac.setAlt(0.0);
+		ac.setLat(0.0);
+		ac.setLon(0.0);
+		ac.setId(0);
+		ac.setTime(time);
+		
+		WifiNetworkExport acExport = new WifiNetworkExport();
+		acExport.setFreuncy(0);
+		acExport.setMAC("test");
+		acExport.setSignal(0);
+		acExport.setSSID("test");
+		
+		List<WifiNetworkExport> acExportList = new ArrayList<>();
+		acExportList.add(acExport);
+		
+		ac.setWifiNetworks(acExportList);
 		
 		List<DataToExport> acList = new ArrayList<>();
 		acList.add(ac);	
 		String actual = DataToExport.buildCSVData(acList);
 		
-		String expected = "55.5";
+		String expected = "Time,ID,Lat,Lon,Alt,#WiFi networks,SSID1,MAC1,Frequncy1,Signal1,SSID2,MAC2,Frequncy2,Signal2,SSID3,MAC3,Frequncy3,Signal3,SSID4,MAC4,Frequncy4,Signal4,SSID5,MAC5,Frequncy5,Signal5,SSID6,MAC6,Frequncy6,Signal6,SSID7,MAC7,Frequncy7,Signal7,SSID8,MAC8,Frequncy8,Signal8,SSID9,MAC9,Frequncy9,Signal9,SSID10,MAC10,Frequncy10,Signal10,\nSat Oct 28 20:10:00 IDT 2017,0,0.0,0.0,0.0,1,test,test,0,0,\n";
 				
 		assertEquals(expected, actual);
 	}
